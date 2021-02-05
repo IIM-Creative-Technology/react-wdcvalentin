f89c74763a0655412d0861dc15ff6bb8525641f8
Valentin <wdcvalentin@yahoo.fr>
2021-02-05 10:21:30 +0100

    [feat]display questions & answers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./pages/Home";
 import { Error } from "./pages/Error";
+import { Home } from "./pages/Home";
+import { QuizzPage } from "./pages/Quizz";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/quizz" component={QuizzPage} />
+        {/* <Route exact path="/quizz-history" component={Quizz} />
+        <Route exact path="/quizz-insolite" component={Quizz} />
+        <Route exact path="/quizz-manga" component={Quizz} /> */}
         <Route component={Error} />
       </Switch>
     </Router>
