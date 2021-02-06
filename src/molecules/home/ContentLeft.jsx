f25f5aff6c76fb90fbbf81d331bc4992646d8758
linhye <46298752+linhye@users.noreply.github.com>
2021-02-06 17:06:03 +0100

    [add] style lp

---
+++ src/molecules/home/ContentLeft.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export function ContentLeft(props) {
+  return (
+    <section>
+      <p className="content_text">
+        ENVIE DE <br /> VOUS DÉTENDRE ? <br />
+        HAPPY QUIZZ EST LÀ <br />
+        POUR VOUS !
+      </p>
+      <input
+        type="button"
+        className="btn_start_quizz"
+        value="Accéder aux quizz"
+        onClick={() => (window.location = "/quizz")}
+      />
+    </section>
+  );
+}
